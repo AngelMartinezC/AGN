3dff98bdd8505bbbe2a7284eb2a57b3663caf4d3
Angel Daniel Martinez Cifuentes <andmartinezci@unal.edu.co>
2019-03-22 10:55:51 -0500

    Update lines_calculations.py

--- lines_calculations.py
+++ lines_calculations.py
@@ -1,4 +1,22 @@
 #-*- coding:utf-8
 
 """
+  Calculation of the area under the spectral flux function
+  for an AGN spectrum.
 """
+
+
+from __future__ import print_function
+from __future__ import division
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+import os
+
+
+data = 'array.fit' #or in .fits format
+
+def read_fits(name):
+  array = 0
