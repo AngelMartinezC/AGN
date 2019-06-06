b33674d00c2b2bb22e195f11194ab897ebada4a7
AngelMartinezC <andmartinezci@unal.edu.co>
2019-06-06 06:54:40 -0500

    Update files

--- agn.py
+++ agn.py
@@ -267,7 +267,7 @@ class agn:
 				Ne = self.ratio(J=self.J2,T=RES,ion=self.ion2)
 				T = self.ratio(J=self.J1,Ne=Ne,ion=self.ion1)
 				if self.show:
-					print("  Iteration {}\n Ne  {}\n T   {}\n".format(i+1,Ne,RES))
+					print("  Iteration {}\n Ne  {}  [part/cm3]\n T   {}  [K]\n".format(i+1,Ne,RES))
 			print(" Done!\n")
 			
 		###########################################################
@@ -283,7 +283,7 @@ class agn:
 				T = self.ratio(J=self.J2,Ne=RES,ion=self.ion2)
 				Ne = ratio(J=J1,T=T,ion=ion1)
 				if self.show:
-					print("  Iteration {}\n Ne  {}\n T   {}\n".format(i+1,RES,T))
+					print("  Iteration {}\n Ne  {}  [part/cm3]\n T   {}  [K]\n".format(i+1,RES,T))
 			print(" Done!\n")
 			
 		#Return Temperature and Density 
@@ -292,39 +292,7 @@ class agn:
 
 
 
-if __name__=='__main__':
 
-	
-	print("---------------------------------\n")
-	print(" PROGRAM FIVEL - PYTHON-TEST\n")
-	i1 = float(input("  Type the number of the ion1\n    1:   O[III]\n    2:   N[II]\n"))
-	if i1 == 1:
-		ion1 = 'OIII'
-	elif i1 == 2:
-		ion1 = 'NII'
-	else: 
-		print("Number not defined")
-		print("Exiting")
-		exit()
-
-	i2 = float(input("  Type the number of the ion2\n    1:   S[II]\n    2:   O[II]\n"))
-	if i2 == 1:
-		ion2 = 'SII'
-	elif i2 == 2:
-		ion2 = 'OII'
-	else: 
-		print("Number not defined")
-		print("Exiting")
-		exit()
-	
-	print("  Input the lines ratio for the {} ion:  ".format(ion1))
-	J1 = float(input())
-	print("  Input the lines ratio for the {} ion:  ".format(ion2))
-	J2 = float(input())
-	
-	A = agn(J1=J1,J2=J2,ion1=ion1,ion2=ion2,show=True)
-	T, Ne = A.fivel()
-	print("  Temperature  {}\n  Density      {}".format(T,Ne))
 	
 	
 
@@ -535,30 +503,151 @@ class spectrum:
 			area.append(lines(i)[0])
 		
 		return area
-	
-	
-	
-if __name__=='__main__':
 
-	data = 'data/spec-1070-52591-0072.fits'
+
+
+
+####################
+
+
+def calculation(name=None, ion1=None, ion2=None, statistics=False, header=False, plot=True, savefig=False, ax=None, show_model=False, iteration=True, plot_spectrum=False,z = 0.00420765,**kwargs):
 	
+	
+	if not name:
+		print('Please provide a .fits file from SDSS. ')
+		print('Exiting...')
+		exit()
+
 	def message():
 		print('-------------------------------------------------------')
-		print(' Code to calculate the fluxes of a set of given lines\n')
+		print(' Code to calculate the Temperature and Density of a set of given lines\n')
 		print('  The user have to select two points of the graphic')
 		print('  corresponding to the value of two wavelengths over')
 		print('  which will be performed a gaussian fit\n')
+		print('  The user will be asked for the lines to work with:')
+		print('   Avaliable ions: ')
+		print('     O[III] or N[II]\n     S[II]  or O[II]')
+		print('  \n  After selecting the two points, press enter to pass the next flux\n')
 		input('  < Press enter to continue > ')
 		print(' ')
-	
+
 	message()
 	
-	data = spectrum(data,header=False,z = 0.00420765)
-	#data.plot()
-	A=data.limits(statistics=False,plot=True,savefig=False)
-	print('\n Area\n  6716:   {0:.3f}\n  6731:   {1:.3f}\
-	\n  4363:   {2:.3f}\n  4959:   {3:.3f}\
-	\n  5007:   {4:.3f}'.format(A[0],A[1],A[2],A[3],A[4]))
+	if not ion1:
+		i1 = float(input("  Type the number of the ion1\n    1:   O[III]\n    2:   N[II]\n"))
+		if i1 == 1:
+			ion1 = 'OIII'
+		elif i1 == 2:
+			ion1 = 'NII'
+		else: 
+			print("Number not defined")
+			print("Exiting")
+			exit()
+	
+	if not ion2:
+		i2 = float(input("  Type the number of the ion2\n    1:   S[II]\n    2:   O[II]\n"))
+		if i2 == 1:
+			ion2 = 'SII'
+		elif i2 == 2:
+			ion2 = 'OII'
+		else: 
+			print("Number not defined")
+			print("Exiting")
+			exit()
+	
+	data = spectrum(data=name,header=header,z=z)
+	
+	if plot_spectrum is True:
+		data.plot()
+	
+	A = data.limits(statistics=statistics,plot=plot,savefig=savefig,ax=ax,show_model=show_model)
+
+	F6716, F6731 = A[0], A[1]
+	F4363, F4959, F5007 = A[2], A[3], A[4]
+
+	F2 = F6716/F6731
+	F1 = (F5007 + F4959)/F4363
+
+
+	A = agn(J1=F1,J2=F2,ion1=ion1,ion2=ion2,show=iteration)
+	T, Ne = A.fivel()
+	
+	return T, Ne
+	
+	
+if __name__=='__main__':
+	
+	
+	def fluxes():
+		data = 'data/spec-1070-52591-0072.fits'
+		
+		def message():
+			print('-------------------------------------------------------')
+			print(' Code to calculate the fluxes of a set of given lines\n')
+			print('  The user have to select two points of the graphic')
+			print('  corresponding to the value of two wavelengths over')
+			print('  which will be performed a gaussian fit\n')
+			input('  < Press enter to continue > ')
+			print(' ')
+		
+		message()
+		
+		data = spectrum(data,header=False,z = 0.00420765)
+		#data.plot()
+		A=data.limits(statistics=False,plot=True,savefig=False)
+		print('\n Area\n  6716:   {0:.3f}\n  6731:   {1:.3f}\
+		\n  4363:   {2:.3f}\n  4959:   {3:.3f}\
+		\n  5007:   {4:.3f}'.format(A[0],A[1],A[2],A[3],A[4]))
+		
+		
+	
+	
+	def fivel():
+		print("---------------------------------\n")
+		print(" PROGRAM FIVEL - PYTHON-TEST\n")
+		i1 = float(input("  Type the number of the ion1\n    1:   O[III]\n    2:   N[II]\n"))
+		if i1 == 1:
+			ion1 = 'OIII'
+		elif i1 == 2:
+			ion1 = 'NII'
+		else: 
+			print("Number not defined")
+			print("Exiting")
+			exit()
+
+		i2 = float(input("  Type the number of the ion2\n    1:   S[II]\n    2:   O[II]\n"))
+		if i2 == 1:
+			ion2 = 'SII'
+		elif i2 == 2:
+			ion2 = 'OII'
+		else: 
+			print("Number not defined")
+			print("Exiting")
+			exit()
+		
+		print("  Input the lines ratio for the {} ion:  ".format(ion1))
+		J1 = float(input())
+		print("  Input the lines ratio for the {} ion:  ".format(ion2))
+		J2 = float(input())
+		
+		A = agn(J1=J1,J2=J2,ion1=ion1,ion2=ion2,show=True)
+		T, Ne = A.fivel()
+		print("  Temperature  {}\n  Density      {}".format(T,Ne))
+	
+	
+	name = 'data/spec-1070-52591-0072.fits'
+	name = 'data/spec-1369-53089-0157.fits'
+	name = 'data/spec-1995-53415-0214.fits'
+	#name = 'data/spec-2128-53800-0577.fits'
+	z = 0.00420765
+	z = 0.00267364 #1369
+	z = 0.022513 #1995
+	#z = 0.0165055 #2128
+	#name = 'data/spec-1369-53089-0157.fits'
+	
+	T, Ne = calculation(name,z=z)
+	
+	
 	
 	exit()
 	
